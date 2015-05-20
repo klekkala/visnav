c38f1a1a8c3857ba602fa401665908cb5f10ee15
kiran4399 <kiran4399@gmail.com>
2015-05-20 18:46:16 +0530

    build commands added

--- ros_setup.sh
+++ ros_setup.sh
@@ -11,4 +11,27 @@ wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo 
 sudo apt-get update
 sudo apt-get upgrade
 
+sudo apt-get install python-setuptools python-pip python-yaml python-argparse python-distribute python-docutils python-dateutil python-setuptools python-six
+sudo pip install rosdep rosinstall_generator wstool rosinstall
 
+###Initializing rosdep
+
+sudo rosdep init
+rosdep update
+
+###Installation
+
+mkdir ~/ros_catkin_ws
+cd ~/ros_catkin_ws
+
+
+###Fetch the core packages so we can build them.
+
+rosinstall_generator ros_comm --rosdistro indigo --deps --wet-only --exclude roslisp --tar > indigo-ros_comm-wet.rosinstall
+wstool init src indigo-ros_comm-wet.rosinstall
+
+
+mkdir ~/ros_catkin_ws/external_src
+sudo apt-get install checkinstall cmake
+sudo sh -c 'echo "deb-src http://mirrordirector.raspbian.org/raspbian/ testing main contrib non-free rpi" >> /etc/apt/sources.list'
+sudo apt-get update
