e2f6dd9cfe55e4e7c398eca1f684cd05925e378a
kiran4399 <kiran4399@gmail.com>
2015-07-29 16:39:33 +0530

    removed+pid+params

--- switch/arduino_switch.ino
+++ switch/arduino_switch.ino
@@ -103,12 +103,17 @@ boolean interruptLock = false;
 
 float ch1, ch2, ch3, ch4, ch5;         // RC channel inputs
 
-unsigned long rcLastChange1 = micros();
-unsigned long rcLastChange2 = micros();
-unsigned long rcLastChange3 = micros();
-unsigned long rcLastChange4 = micros();
-unsigned long rcLastChange5 = micros();
-
+const unsigned int defaultPulseWidths[CHANNELS] = {
+  MIN_PULSE_TIME,      // Throttle
+  HALF_PULSE_TIME,     // Roll
+  HALF_PULSE_TIME,     // Pitch
+  MIN_PULSE_TIME,      // Yaw
+  
+  MAX_PULSE_TIME,     // AUX1 (MODE in Aeroquad)
+  MAX_PULSE_TIME,     // AUX2 (ALTITUDE in Aeroquad)
+  MAX_PULSE_TIME,     // AUX3
+  MAX_PULSE_TIME      // AUX4
+};
 
 
 /** Motor control variables **/
@@ -174,51 +179,6 @@ void loop(){
   
 }
 
-/*  computePID function
- *
- *  formats data for use with PIDLib
- *  and computes PID output
- */
-
-void computePID(){
-
-  acquireLock();
-  
-  ch1 = floor(ch1/RC_ROUNDING_BASE)*RC_ROUNDING_BASE;
-  ch2 = floor(ch2/RC_ROUNDING_BASE)*RC_ROUNDING_BASE;
-  ch4 = floor(ch4/RC_ROUNDING_BASE)*RC_ROUNDING_BASE;
-
-  ch2 = map(ch2, RC_LOW_CH2, RC_HIGH_CH2, PITCH_MIN, PITCH_MAX);
-  ch1 = map(ch1, RC_LOW_CH1, RC_HIGH_CH1, ROLL_MIN, ROLL_MAX);
-  ch4 = map(ch4, RC_LOW_CH4, RC_HIGH_CH4, YAW_MIN, YAW_MAX);
-  
-  if((ch2 < PITCH_MIN) || (ch2 > PITCH_MAX)) ch2 = ch2Last;
-  if((ch1 < ROLL_MIN) || (ch1 > ROLL_MAX)) ch1 = ch1Last;
-  if((ch4 < YAW_MIN) || (ch4 > YAW_MAX)) ch4 = ch4Last;
-  
-  ch1Last = ch1;
-  ch2Last = ch2;
-  ch4Last = ch4;
-  
-  ypr[0] = ypr[0] * 180/M_PI;
-  ypr[1] = ypr[1] * 180/M_PI;
-  ypr[2] = ypr[2] * 180/M_PI;
-  
-  if(abs(ypr[0]-yprLast[0])>30) ypr[0] = yprLast[0];
-  if(abs(ypr[1]-yprLast[1])>30) ypr[1] = yprLast[1];
-  if(abs(ypr[2]-yprLast[2])>30) ypr[2] = yprLast[2];
-  
-  yprLast[0] = ypr[0];
-  yprLast[1] = ypr[1];
-  yprLast[2] = ypr[2];
-
-  pitchReg.Compute();
-  rollReg.Compute();
-  yawReg.Compute();
-  
-  releaseLock();
-
-}
 
 
 /*  calculateVelocities function
