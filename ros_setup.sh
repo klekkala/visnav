bbca0c1614d02af36635856299a8503706be33a9
kiran4399 <kiran4399@gmail.com>
2015-08-17 16:56:25 +0530

    ros_setup.sh+update+

--- ros_setup.sh
+++ ros_setup.sh
@@ -9,7 +9,6 @@
 ################################################################Resolving Dependencies#########################################################
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu wheezy main" > /etc/apt/sources.list.d/ros-latest.list'
-
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get upgrade
