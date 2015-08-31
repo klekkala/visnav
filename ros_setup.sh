6afcc31994c110b691203fc896379d99ac20d375
kiran4399 <kiran4399@gmail.com>
2015-08-31 23:21:39 +0530

    removed+sudo+

--- ros_setup.sh
+++ ros_setup.sh
@@ -8,17 +8,18 @@
 
 ################################################################Resolving Dependencies#########################################################
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu wheezy main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo -s
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu wheezy main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
-sudo apt-get update
-sudo apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
-sudo apt-get install python-setuptools python-pip python-yaml python-argparse python-distribute python-docutils python-dateutil python-setuptools python-six
-sudo pip install rosdep rosinstall_generator wstool rosinstall
+apt-get install python-setuptools python-pip python-yaml python-argparse python-distribute python-docutils python-dateutil python-setuptools python-six
+pip install rosdep rosinstall_generator wstool rosinstall
 
 ###Initializing rosdep
 
-sudo rosdep init
+rosdep init
 rosdep update
 
 ###Installation
@@ -34,23 +35,23 @@ wstool init src indigo-ros_comm-wet.rosinstall
 
 
 mkdir ~/ros_catkin_ws/external_src
-sudo apt-get install checkinstall cmake -y
-sudo sh -c 'echo "deb-src http://mirrordirector.raspbian.org/raspbian/ testing main contrib non-free rpi" >> /etc/apt/sources.list'
-sudo apt-get update
+apt-get install checkinstall cmake -y
+sh -c 'echo "deb-src http://mirrordirector.raspbian.org/raspbian/ testing main contrib non-free rpi" >> /etc/apt/sources.list'
+apt-get update
 
 ###Build libconsole-bridge-dev
 
 cd ~/ros_catkin_ws/external_src
-sudo apt-get build-dep console-bridge
+apt-get build-dep console-bridge
 apt-get source -b console-bridge
-sudo dpkg -i libconsole-bridge0.2_*.deb libconsole-bridge-dev_*.deb
+dpkg -i libconsole-bridge0.2_*.deb libconsole-bridge-dev_*.deb
 
 
 ###Build liblz4-dev
 
 cd ~/ros_catkin_ws/external_src
 apt-get source -b lz4
-sudo dpkg -i liblz4-*.deb
+dpkg -i liblz4-*.deb
 
 ###liburdfdom-headers-dev
 
@@ -58,26 +59,26 @@ cd ~/ros_catkin_ws/external_src
 git clone https://github.com/ros/urdfdom_headers.git
 cd urdfdom_headers
 cmake .
-sudo checkinstall make install
+checkinstall make install
 	
 ###liburdfdom-dev:
 
 cd ~/ros_catkin_ws/external_src
-sudo apt-get install libboost-test-dev libtinyxml-dev -y
+apt-get install libboost-test-dev libtinyxml-dev -y
 git clone https://github.com/ros/urdfdom.git
 cd urdfdom
 cmake .
-sudo checkinstall make install
+checkinstall make install
 
 ###collada-dom-dev:
 
 cd ~/ros_catkin_ws/external_src
-sudo apt-get install libboost-filesystem-dev libxml2-dev -y
+apt-get install libboost-filesystem-dev libxml2-dev -y
 wget http://downloads.sourceforge.net/project/collada-dom/Collada%20DOM/Collada%20DOM%202.4/collada-dom-2.4.0.tgz
 tar -xzf collada-dom-2.4.0.tgz
 cd collada-dom-2.4.0
 cmake .
-sudo checkinstall make install
+checkinstall make install
 
 
 ####################Making the cat-kin workspace#######################
@@ -86,7 +87,7 @@ cd ~/ros_catkin_ws
 rosdep install --from-paths src --ignore-src --rosdistro indigo -y -r --os=debian:wheezy
 
 
-sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/indigo
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/indigo
 
 ###################Adding released packages##########################
 
@@ -99,4 +100,4 @@ wstool update -t src
 
 rosdep install --from-paths src --ignore-src --rosdistro indigo -y -r --os=debian:wheezy
 
-sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/indigo
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/indigo
