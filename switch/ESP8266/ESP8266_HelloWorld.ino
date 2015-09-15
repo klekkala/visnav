437bf6b90a5140c0c278fd6f520cf8b93f06e4c4
kiran4399 <kiran4399@gmail.com>
2015-09-15 21:44:59 +0530

    additions++

--- switch/ESP8266/ESP8266_HelloWorld.ino
+++ switch/ESP8266/ESP8266_HelloWorld.ino
@@ -317,18 +317,10 @@ void loop(){
   }
   calculateVelocities();
   pin_pwm();
-SoftwareSerial portOne(10, 11);
-
-// software serial #2: TX = digital pin 8, RX = digital pin 9
-// on the Mega, use other pins instead, since 8 and 9 don't work on the Mega
-SoftwareSerial portTwo(8, 9);
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
 
 
   // Start each software serial port
